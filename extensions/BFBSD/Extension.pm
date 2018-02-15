2c6fc2eaed3de80341328a1a95d66991968f8804
Oleksandr Tymoshenko <oleksandr.tymoshenko@dolby.com>
2018-02-14 19:24:01 -0800

    [218997] Let reporter edit keywords after submission
    Reporter can edit keywords when submitting bug so there is no
    reason why he/she can't edit them when editing PR. Previous
    logic was only members of editbugs group could edit keywords

--- extensions/BFBSD/Extension.pm
+++ extensions/BFBSD/Extension.pm
@@ -39,7 +39,9 @@ sub bug_check_can_change_field {
     my ($self, $args) = @_;
     if ($args->{'field'} eq 'keywords') {
         my $user = Bugzilla->user;
-        if (!$user->in_group('editbugs', $args->{'bug'}->product_id)) {
+        my $reporter = $args->{'bug'}->reporter->id;
+        if (($user->id != $reporter) &&
+            !$user->in_group('editbugs', $args->{'bug'}->product_id)) {
             push(@{ $args->{'priv_results'} }, PRIVILEGES_REQUIRED_EMPOWERED);
             return;
         }
